6ddeb1594e112757e90ee456b583fe39761c4cda
mitkooooooooooooooooooooooo <2008mitko@gmail.com>
2026-05-31 14:39:14 +0300

    Views

---
+++ Cars/Controllers/DealerController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Cars.Data;
+using Cars.Data.Models;
+
+namespace Cars.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class DealerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public DealerController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Create()
+        {
+            ViewBag.Categories = _context.Categories.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Car car)
+        {
+            var userId = _userManager.GetUserId(User);
+            var dealer = _context.Dealers.FirstOrDefault(d => d.UserId == userId);
+
+            if (dealer != null)
+            {
+                car.DealerId = dealer.Id;
+                car.IsSold = false;
+
+                _context.Cars.Add(car);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(car);
+        }
+    }
+}
---
+++ Cars/Controllers/ShopController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Cars.Data;
+
+namespace Cars.Controllers
+{
+    [Authorize(Roles = "User")]
+    public class ShopController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Buy(int id)
+        {
+            var car = await _context.Cars.FindAsync(id);
+            if (car != null && !car.IsSold)
+            {
+                car.IsSold = true;
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
